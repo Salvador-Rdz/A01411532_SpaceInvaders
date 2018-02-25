e94532afc3dd561eb14b97f3d9b2abfa3ec9578a
Salvador-Rdz <rodriguez.salazar.salvador@gmail.com>
2018-02-25 17:19:56 -0600

    Base Scripts Modified

---
+++ Assets/AsteroidController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour {
+    public float projectileSpeed = 0.0001f;
+    public Rigidbody2D rgb;
+    public float damage = 30;
+    public float degreesPerSecond = 50.0f; 
+    // Use this for initialization
+    public void Awake()
+    {
+        rgb =gameObject.GetComponent<Rigidbody2D>();
+        rgb.velocity = new Vector2(0, -projectileSpeed);
+    }
+    void Start () {
+		
+	}
+	
+    public void Crash()
+    {
+        print("Should be destroyed");
+        Destroy(gameObject);
+    }
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, 50 * Time.deltaTime);
+    }
+    public float getDamage()
+    {
+        return this.damage;
+    }
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;				//	We need this in order to handle the Text object
+
+public class ScoreKeeper : MonoBehaviour {
+    public GameObject bossFormation;
+    public GameObject enemyFormation;
+	public static int score = 0;			//	Player's score
+	public Text scoreText;					//	So we can modify the score's Text
+
+	// Use this for initialization
+	void Start () {
+		Reset ();
+		scoreText.text = score.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(score >= 150*14)
+        {
+            bossFormation.SetActive(true);
+            enemyFormation.SetActive(false);
+        }
+	}
+
+	public void Score(int points) {
+		score += points;
+		scoreText.text = score.ToString ();
+	}
+
+	public static void Reset() {
+		score = 0;
+		// scoreText.text = score.ToString ();
+	}
+}
